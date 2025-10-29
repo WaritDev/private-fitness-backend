aebcb1352be8d4a667401564ee61e6492705f399
WaritDev <WaritTheProgrammer@gmail.com>
2025-10-29 22:14:23 +0700

    refactor(config): unify DB config to support MariaDB (DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)

--- config/config.go
+++ config/config.go
@@ -2,32 +2,44 @@ package config
 
 import (
 	"log"
+	"strings"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Port             string `mapstructure:"PORT"`
-	PostgresHost     string `mapstructure:"POSTGRES_HOST"`
-	PostgresUser     string `mapstructure:"POSTGRES_USER"`
-	PostgresPassword string `mapstructure:"POSTGRES_PASSWORD"`
-	PostgresDB       string `mapstructure:"POSTGRES_DB"`
-	PostgresPort     string `mapstructure:"POSTGRES_PORT"`
-	Schema           string `mapstructure:"SCHEMA"`
-	UseDocker        bool   `mapstructure:"USE_DOCKER"`
+	Port       string `mapstructure:"PORT"`
+	DBDriver   string `mapstructure:"DB_DRIVER"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBParams   string `mapstructure:"DB_PARAMS"`
+	UseDocker  bool   `mapstructure:"USE_DOCKER"`
+	TZ         string `mapstructure:"TZ"`
 }
 
 func ProvideConfig() *Config {
-	config := &Config{}
-	viper.SetConfigFile(".env")
+	v := viper.New()
+	v.SetConfigFile(".env")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalln("Error reading config file", err)
+	v.SetDefault("DB_DRIVER", "mysql")
+	v.SetDefault("DB_HOST", "localhost")
+	v.SetDefault("DB_PORT", "3307")
+	v.SetDefault("DB_PARAMS", "parseTime=true&loc=Asia%2FBangkok&charset=utf8mb4")
+	v.SetDefault("TZ", "Asia/Bangkok")
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Println("⚠️  No .env file found, using env vars/defaults:", err)
 	}
 
-	if err := viper.Unmarshal(config); err != nil {
+	cfg := &Config{}
+	if err := v.Unmarshal(cfg); err != nil {
 		log.Fatalln("Unable to decode into struct", err)
 	}
 
-	return config
+	return cfg
 }
